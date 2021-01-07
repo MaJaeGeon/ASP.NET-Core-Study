50332e4820ed1f41a5a63c7a3b036df8e9fdb4a1
MaJaeGeon <akworjs0517@gmail.com>
2021-01-07 16:17:50 +0900

    RankingApp

---
+++ Inflearn/RankingApp-Rookiss/RankingApp-Rookiss/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RankingApp_Rookiss.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RankingApp_Rookiss.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<GameResult> GameResult { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ Inflearn/RankingApp-Rookiss/RankingApp-Rookiss/Data/Services/RankingService.cs
@@ -0,0 +1,72 @@
+﻿using RankingApp_Rookiss.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankingApp_Rookiss.Data.Services
+{
+    public class RankingService
+    {
+        ApplicationDbContext _context;
+
+        public RankingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // CREATE
+        public Task<GameResult> AddGameResult(GameResult gameResult)
+        {
+            _context.GameResult.Add(gameResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(gameResult);
+        }
+
+        // READ
+        public Task<List<GameResult>> GetGameResultsAsync()
+        {
+            List<GameResult> results = _context.GameResult
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return Task.FromResult(results);
+        }
+
+        // UPDATE
+        public Task<bool> UpdateGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResult
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return Task.FromResult(false);
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
+        }
+
+        // DELETE
+        public Task<bool> DeleteGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResult
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return Task.FromResult(false);
+
+            _context.GameResult.Remove(gameResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
+        }
+    }
+}
